d00278787f1eb60f96b3a8b9bc25113d741474fc
Chris Marchesi <cmarchesi@paybyphone.com>
2017-01-16 13:42:51 -0800

    Fix README example

--- main.tf
+++ main.tf
@@ -21,32 +21,32 @@
  * 
  * Usage example:
  *
- * module "vpc" {
- *   source                  = "github.com/paybyphone/terraform_aws_vpc?ref=VERSION"
- *   project_path            = "your/project"
- *   public_subnet_addresses = ["10.0.0.0/26", "10.0.0.64/26"]
- *   vpc_network_address     = "10.0.0.0/24"
- * }
- * 
- * module "private_subnets" {
- *   source                            = "github.com/paybyphone/terraform_aws_private_subnet?ref=VERSION"
- *   nat_gateway_count                 = "2"
- *   private_subnet_addresses          = ["10.0.0.128/26", "10.0.0.192/26"]
- *   private_subnet_availability_zones = "${values(module.vpc.public_subnet_availability_zones)}"
- *   project_path                      = "your/project"
- *   public_subnet_ids                 = "${keys(module.vpc.public_subnet_availability_zones)}"
- *   vpc_id                            = "${module.vpc.vpc_id}"
- * }
- * 
- * module "vpn" {
- *   source                    = "git::ssh://git@stash.dc0.verrus.net/tfm/paybyphone-vpn.git?ref=v0.1.0"
- *   private_route_table_count = "2"
- *   private_route_table_ids   = "${module.private_subnets.private_route_table_ids}"
- *   project_path              = "your/project"
- *   remote_network_addresses  = ["192.168.0.0/24", "192.168.1.0/24"]
- *   vpc_id                    = "${module.vpc.vpc_id}"
- *   vpn_ip_address            = "10.9.8.7"
- * }
+ *     module "vpc" {
+ *       source                  = "github.com/paybyphone/terraform_aws_vpc?ref=VERSION"
+ *       project_path            = "your/project"
+ *       public_subnet_addresses = ["10.0.0.0/26", "10.0.0.64/26"]
+ *       vpc_network_address     = "10.0.0.0/24"
+ *     }
+ *     
+ *     module "private_subnets" {
+ *       source                            = "github.com/paybyphone/terraform_aws_private_subnet?ref=VERSION"
+ *       nat_gateway_count                 = "2"
+ *       private_subnet_addresses          = ["10.0.0.128/26", "10.0.0.192/26"]
+ *       private_subnet_availability_zones = "${values(module.vpc.public_subnet_availability_zones)}"
+ *       project_path                      = "your/project"
+ *       public_subnet_ids                 = "${keys(module.vpc.public_subnet_availability_zones)}"
+ *       vpc_id                            = "${module.vpc.vpc_id}"
+ *     }
+ *     
+ *     module "vpn" {
+ *       source                    = "git::ssh://git@stash.dc0.verrus.net/tfm/paybyphone-vpn.git?ref=v0.1.0"
+ *       private_route_table_count = "2"
+ *       private_route_table_ids   = "${module.private_subnets.private_route_table_ids}"
+ *       project_path              = "your/project"
+ *       remote_network_addresses  = ["192.168.0.0/24", "192.168.1.0/24"]
+ *       vpc_id                    = "${module.vpc.vpc_id}"
+ *       vpn_ip_address            = "10.9.8.7"
+ *     }
  * 
  */
 
